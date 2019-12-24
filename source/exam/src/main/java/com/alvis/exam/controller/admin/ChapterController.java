b92c10d277732f8f03f118b38ad54bc0ab07290a
yangsy01 <We520304>
2019-12-24 09:56:34 +0800

    11

--- source/exam/src/main/java/com/alvis/exam/controller/admin/ChapterController.java
+++ source/exam/src/main/java/com/alvis/exam/controller/admin/ChapterController.java
@@ -5,6 +5,7 @@ import com.alvis.exam.base.RestResponse;
 import com.alvis.exam.domain.Chapter;
 import com.alvis.exam.service.ChapterService;
 import com.alvis.exam.viewmodel.admin.article.ArticleTypeVM;
+import com.alvis.exam.viewmodel.admin.user.ChapterAndIdVM;
 import com.alvis.exam.viewmodel.admin.user.ChapterVM;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,9 +32,15 @@ public class ChapterController extends BaseApiController {
      * @return
      */
     @RequestMapping(value = "/insertChapter", method = RequestMethod.POST)
-    public RestResponse<Chapter> insert(@RequestBody ChapterVM model){
+    public RestResponse<Chapter> insert(@RequestBody ChapterAndIdVM model){
 
-        this.chapterService.insertChapter(model);
+        ChapterVM model1= new ChapterVM();
+        model1.setName(model.getChapterVM().getName());
+        model1.setTypeId(model.getTypeId());
+        model1.setSequence(model.getChapterVM().getSequence());
+        model1.setCriterion(model.getChapterVM().getCriterion());
+        model1.setState(1);
+        this.chapterService.insertChapter(model1);
         return RestResponse.ok();
     }
 
