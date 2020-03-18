0e906a96eb8c132c1cdd8a4f16603919d6e3cb80
lz-yy <2475214987@qq.com>
2020-03-18 15:11:06 +0800

    3.18

--- out/production/main/com/alvis/exam/viewmodel/admin/article/PageInfoVM.java
+++
@@ -1,9 +0,0 @@
-package com.alvis.exam.viewmodel.admin.article;
-
-import com.github.pagehelper.PageInfo;
-import lombok.Data;
-
-@Data
-public class PageInfoVM<T> extends PageInfo {
-    private String type;
-}
