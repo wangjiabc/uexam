e0c102088e07035c70f23b8a838e309009017f01
changshan <401270884@qq.com>
2020-03-19 14:52:21 +0800

    3.19_zsc_04

--- source/exam/src/main/java/com/alvis/exam/repository/DataQueryMapper.java
+++ source/exam/src/main/java/com/alvis/exam/repository/DataQueryMapper.java
@@ -45,7 +45,7 @@ public interface DataQueryMapper extends BaseMapper<SourcePut> {
     /**
      * 查询阶段完成进度
      */
-    @Select("select a.real_name,(b.orderNumber/a.quarter_sale_norm)*100 from t_user a, wide_narrow b where a.user_uuid=b.user_id and b.user_id='${uuid}'")
+    @Select("select a.real_name,(b.orderNumber/a.quarter_sale_norm)*100 as stage from t_user a, wide_narrow b where a.user_uuid=b.user_id and b.user_id='${uuid}'")
     List<Map<String,Object>> queryCompletionStage(@Param("uuid") String uuid);
 
     /**
@@ -83,6 +83,6 @@ public interface DataQueryMapper extends BaseMapper<SourcePut> {
     /**
      * 查询阶段完成进度
      */
-    @Select("select a.real_name,(b.orderNumber/a.quarter_sale_norm)*100 from t_user a, wide_narrow b where a.user_uuid=b.user_id and b.user_id='${uuid}'")
+    @Select("select a.real_name,(b.orderNumber/a.quarter_sale_norm)*100 as stage from t_user a, wide_narrow b where a.user_uuid=b.user_id and b.user_id='${uuid}'")
     List<Map<String,Object>> queryCompletionStageMGE(@Param("uuid") String uuid);
 }
