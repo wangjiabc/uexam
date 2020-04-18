cb1a8a23d5e5cb5a0a988bb57321c8c32e9f42c3
yangsy01 <We520304>
2020-04-18 15:06:46 +0800

    参数

--- source/exam/src/main/java/com/alvis/exam/domain/cashregister.java
+++ source/exam/src/main/java/com/alvis/exam/domain/cashregister.java
@@ -6,7 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Table;
 
 @Table(name = "cashregister")
-public class cashregister extends MessageRequestVM {
+public class cashregister extends MessageRequestVM{
     @Column(name = "id")
     private Integer id;
 
@@ -25,5 +25,52 @@ public class cashregister extends MessageRequestVM {
     @Column(name = "wrapSmokeNumber")
     private Integer wrapSmokeNumber;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getShoopName() {
+        return shoopName;
+    }
+
+    public void setShoopName(String shoopName) {
+        this.shoopName = shoopName;
+    }
+
+    public String getCustomerCode() {
+        return customerCode;
+    }
+
+    public void setCustomerCode(String customerCode) {
+        this.customerCode = customerCode;
+    }
+
+    public Integer getSaleNumber() {
+        return saleNumber;
+    }
+
+    public void setSaleNumber(Integer saleNumber) {
+        this.saleNumber = saleNumber;
+    }
+
+    public Integer getStripSmokeNumber() {
+        return stripSmokeNumber;
+    }
+
+    public void setStripSmokeNumber(Integer stripSmokeNumber) {
+        this.stripSmokeNumber = stripSmokeNumber;
+    }
+
+    public Integer getWrapSmokeNumber() {
+        return wrapSmokeNumber;
+    }
+
+    public void setWrapSmokeNumber(Integer wrapSmokeNumber) {
+        this.wrapSmokeNumber = wrapSmokeNumber;
+    }
 
 }
